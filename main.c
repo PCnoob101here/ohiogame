a73a29a9330c7f478e03750030cb0f51b97c605c
PCnoob101here <rr2009maple@gmail.com>
2024-12-01 16:06:30 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,46 @@
+//i wanted to make an app made solely by antifurries, thats what this is
+#include <stdio.h>
+#include <stdlib.h>
+#include <freeglut.h>
+
+char theme[];
+char *uidata[12];
+/*
+1 ver
+2 bordrrgb
+3 bckrndrgb
+4 btn0pnts
+5 btn0rgb
+6 btn1pnts
+7 btn1rgb
+8 slidr0pnts
+9 slidr0rgb
+10 slidr1pnts
+11 slidr1rgb
+12 comboxpnts
+13 comboxrgb
+----
+amnt mix
+r g b cords
+*/
+
+void getheme(){
+FILE *fl = fopen(theme, "r+");
+if(fl = NULL)
+{
+    return 0;
+}
+
+fclose(fl);
+return 0;
+}
+
+int main()
+{
+    glutInit(&argc, argv);
+    glutCreateWindow("madebyantifurs");
+    glutInitWindowSize(CW_USEDEFAULT, CW_USEDEFAULT);
+    glutInitWindowPosition(1, 1);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutMainLoop();
+}
