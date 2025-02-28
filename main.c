01f3fe244473288ccf20679b8f839c0702dbcdb8
PCnoob101here <rr2009maple@gmail.com>
2025-02-27 21:06:43 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,63 +1,76 @@
-//i wanted to make an app made solely by antifurries, thats what this is
-#include <stdio.h>
-#include <stdlib.h>
-#include <glut.h>
+#include <windows.h>
 
-char theme[];
-char *uidata[12];
-/*
-1 ver
-2 bordrrgb
-3 bckrndrgb
-4 btn0pnts
-5 btn0rgb
-6 btn1pnts
-7 btn1rgb
-8 slidr0pnts
-9 slidr0rgb
-10 slidr1pnts
-11 slidr1rgb
-12 comboxpnts
-13 comboxrgb
-----
-amnt mix
-r g b cords
-*/
+const char g_szClassName[] = "myWindowClass";
 
-void getheme(){
-FILE *fl = fopen(theme, "r+");
-if(fl = NULL)
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
     return 0;
 }
-char head = 0;
-char step = 0;
-while(step == )
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
 {
-    fseek(fl, head, SEEK_SET);
-    switch(step)
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
     {
-        case 0: head = 1;
-                step = 1;
-                break;
-        case 1: calloc()
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
-}
-fclose(fl);
-return 0;
-}
 
-void scene(){
-    glbegin();
-    glend();
-}
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "The title of my window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        NULL, NULL, hInstance, NULL);
 
-int main()
-{
-    glutInit(&argc, argv);
-    glutCreateWindow("madebyantifurs");
-    glutInitWindowSize(CW_USEDEFAULT, CW_USEDEFAULT);
-    glutInitWindowPosition(1, 1);
-    glutInitDisplayMode(GLUT_RGBA);
-    glutMainLoop();
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Step 3: The Message Loop
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
 }
