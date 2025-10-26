e20bfbc4b89863ab51991664e60800cc6f46e37a
PCnoob101here <rr2009maple@gmail.com>
2025-10-26 09:41:49 -0700

    Update and rename spristnames.h to afnames1.h

---
+++ ohiodemo1/afnames1.h
@@ -0,0 +1,33 @@
+#ifndef AFGLLISTS100_H_INCLUDED
+#define AFGLLISTS100_H_INCLUDED
+//glList names
+#define afInvertY 1
+#define afButton 2
+#define afCheckBox 3
+#define afDeodorant 4
+#define afGrass 5
+#define afInvertX 6
+#define afFurryHead 7
+#define afPoop 8
+#define afStillWater 9
+#define afFurryTorso 10
+#define afTestCube 11
+#define afFurryPaw 12
+#define afFurryArm1 13
+#define afFurryArm2 14
+#define afFurryLeg1 15
+#define afFurryLeg2 16
+#define afStraightFlag 17
+#define afFillRender 18
+//npc types
+#define AF_CYAN_FURRY
+#define AF_GREY_FURRY
+#define AF_ORANGE_FURRY
+#define AF_PINK_FURRY
+#define AF_GREEN_FURRY
+#define AF_SUS_VAN
+#define AF_SLENDER_MAN
+#define AF_JEFF_THE_KILLER
+#define AF_ANTIFURRY
+
+#endif // AFGLLISTS100_H_INCLUDED
